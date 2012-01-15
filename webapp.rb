987ac369435504d38cc8b760658745411643eb7b
Mao Tsunekawa <tsunekaw@slis.tsukuba.ac.jp>
2012-01-16 00:19:30 +0900

    create index page and bookmarklet

--- webapp.rb
+++ webapp.rb
@@ -2,14 +2,20 @@ $LOAD_PATH << File.expand_path(File.dirname(__FILE__))
 require 'sinatra'
 require './lib/ndl_search'
 
+before do
+  @bibtex_path = request.host+(request.port!=80 ? ":#{request.port}":"")+"/bibtex"
+end
+
 get '/' do
-  "hello"
+  erb :index
 end
 get '/bibtex' do
   content_type 'text/plain'
 
-  id = params[:id]
-  @item = NdlSearch::Item.import(id)
-  p @item
-  erb @item.media_type.to_sym
+  ids = Array.new
+  ids << params[:id] unless params[:id].nil?
+  ids << params[:url].split("/").last unless params[:url].nil?
+  
+  @items = ids.map {|id| NdlSearch::Item.import(id) }
+  erb :bibtex
 end
